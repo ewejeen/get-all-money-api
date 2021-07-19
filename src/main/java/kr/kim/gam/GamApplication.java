85db9b28da463ab5d737bbdf09f07f1d9b6cc951
ewejeen <yoojinkim24@gmail.com>
2021-07-20 00:05:03 +0900

    크롤링 테스트코드

--- src/main/java/kr/kim/gam/GamApplication.java
+++ src/main/java/kr/kim/gam/GamApplication.java
@@ -2,8 +2,10 @@ package kr.kim.gam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GamApplication {
 
 	public static void main(String[] args) {
